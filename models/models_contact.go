26ad399b28fe2dacca82d3a8b44aac30e90429bb
Gabriel SULLICE <gabriel@sullice.com>
2016-02-13 09:58:27 -0700

    Add more contact fields

--- models/models_contact.go
+++ models/models_contact.go
@@ -8,11 +8,15 @@ type Phone struct {
 
 type Contact struct {
 	ID        string   `gorethink:"id,omitempty" json:"-"`
+	Name      string   `gorethink:"name" json:"name"`
 	FirstName string   `gorethink:"firstName" json:"firstName"`
 	LastName  string   `gorethink:"lastName" json:"lastName"`
+	Synonyms  []string `gorethink:"synonyms" json:"synonyms"`
 	Position  string   `gorethink:"position" json:"position"`
 	Phones    []Phone  `gorethink:"phones" json:"phones"`
 	Emails    []string `gorethink:"emails" json:"emails"`
+	Notes     string   `gorethink:"notes" json:"notes"`
+	Related   []string `gorethink:"related" json:"related"`
 }
 
 func (c Contact) GetName() string {
